f96aae6b73f99349ddb9c627165c180101dff453
John Kevin Basco <basco.johnkevin@gmail.com>
2014-09-21 12:04:13 +0800

    add more tests for UserTodoController

--- app/tests/unit/LaraTodo/User/Controllers/UserTodoControllerTest.php
+++ app/tests/unit/LaraTodo/User/Controllers/UserTodoControllerTest.php
@@ -3,19 +3,42 @@
 use TestCase;
 use User;
 use Hash;
+use Todo;
 
 class UserTodoControllerTest extends TestCase
 {
-    public function testIndexShouldRespondWithOk()
+    protected $user;
+
+    public function setUp()
     {
+        parent::setUp();
+
         $user = new User;
         $user->username = 'johndoe25';
         $user->password = 'dummypassword';
         $user->email = 'johndoe25@gmail.com';
         $user->save();
+        $this->user = $user;
+    }
 
-        $crawler = $this->client->request('GET', route('users.todos.index', $user->username));
+    public function testIndexShouldRespondWithOk()
+    {
+        $crawler = $this->client->request('GET', route('users.todos.index', $this->user->username));
         $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+    public function testIndexShouldShowUsernameOfTheTodoListOwner()
+    {
+        $crawler = $this->client->request('GET', route('users.todos.index', $this->user->username));
+        $this->assertViewHas('usernameOfTodoListOwner', $this->user->username);
+    }
+
+    public function testIndexShouldShowTodos()
+    {
+        $todos = Todo::all();
+
+        $crawler = $this->client->request('GET', route('users.todos.index', $this->user->username));
+        $this->assertViewHas('todos', $todos);
     }
 
 
